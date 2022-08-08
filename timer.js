613d6cb195dc526644498472c19afa4c6b9c9ba9
Antanas Grigaitis <100243851+antyg123@users.noreply.github.com>
2022-08-07 21:09:06 -0500

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,5 +1,38 @@
-let [hours, minutes] = [0,0];
-let timerRef = document.querySelector('.mainTime');
-let int = null;
-let number = document.getElementById('number');
-setInterval(number, 1000);
+
+//function start(){
+  //  setInterval(increaseNumber, 1000);
+    //function increaseNumber(){
+      //  let number = parseInt(document.getElementById('time').innerText);
+        //while(number<10){
+          //  number++;
+        //}
+    //}
+//}
+const number = setInterval(printX, 1000);
+while (x<10){
+    function printX(x){
+        x=0;
+        while(x<10){
+            console.log(x);
+            x++;
+    }
+}}
+
+
+//create a funtion that upon clicking, will count up to ten.
+//VARIABLES:
+//  number that will increase
+//FUNCTIONS:
+//  button start
+//  interval 
+//  add 1 to a number
+
+//BUTTON START
+//  button is clicked
+//  interval begins
+
+//INTERVAL
+//  declare number
+//  number will go up by 1 
+//  interval takes place over one second
+//  interval will last until number hits ten*optional
